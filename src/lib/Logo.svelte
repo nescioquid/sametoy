a50909f6c6281c4dbcdedff78425456861b01677
nescioquid <info@creachr.com>
2025-12-08 18:13:39 -0500

    darkens up the logo

--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -1,4 +1,4 @@
-<h2 class="text-center tracking-widest text-purple-900 text-[4vh] opacity-70">
+<h2 class="text-center tracking-widest text-gray-700 text-[4vh] opacity-50">
   <span class="font-semibold">SAME TOY</span>
   <span class="font-light"><em>PROGRESS</em></span>
 </h2>
