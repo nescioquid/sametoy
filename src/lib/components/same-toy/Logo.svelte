9cdcd2e7769bf2b7e5e0cf8b5130d9227bc3650b
nescioquid <info@creachr.com>
2025-12-09 17:45:00 -0500

    adds some padding for spacing one the logo

--- src/lib/components/same-toy/Logo.svelte
+++ src/lib/components/same-toy/Logo.svelte
@@ -1,4 +1,4 @@
-<h2 class="text-center tracking-widest text-[4vh] opacity-70">
+<h2 class="text-center tracking-widest text-[4vh] opacity-70 pb-1">
   <span class="font-semibold text-gray-800">SAME TOY</span>
   <span class="font-light text-base-100"><em>PROGRESS</em></span>
 </h2>
