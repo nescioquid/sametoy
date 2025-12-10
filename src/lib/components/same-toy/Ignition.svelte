5fc4e856d18e1f2602dda4a0645924b6f8cfc82c
nescioquid <info@creachr.com>
2025-12-10 12:20:13 -0500

    renames PowerOn component to Ignition, adds keyboard functionality

---
+++ src/lib/components/same-toy/Ignition.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Dot } from '@lucide/svelte'
+  import { power, togglePower } from '$lib/power.svelte'
+
+  const pressIgnition = () => togglePower()
+
+  const handlers = {
+    q: pressIgnition,
+  }
+
+  function onkeyup(event) {
+    const key = event.key.toLowerCase()
+    handlers[key]?.()
+  }
+</script>
+
+<svelte:window {onkeyup} />
+
+<button
+  onclick={pressIgnition}
+  class={`btn btn-soft btn-circle rounded-full opacity-85 w-[12vh] h-[7vh] border-[1vh] ${
+    power.onOff ? 'border-info' : 'border-warning'
+  }`}
+>
+  <Dot class={`opacity-50 size-[6vh] ${power.onOff ? 'text-success' : ''}`} />
+</button>
--- src/lib/components/same-toy/SameToy.svelte
+++ src/lib/components/same-toy/SameToy.svelte
@@ -3,7 +3,7 @@
   import DPad from '$lib/components/same-toy/DPad.svelte'
   import Housing from '$lib/components/same-toy/Housing.svelte'
   import Logo from '$lib/components/same-toy/Logo.svelte'
-  import PowerOn from '$lib/components/same-toy/PowerOn.svelte'
+  import Ignition from '$lib/components/same-toy/Ignition.svelte'
   import Screen from '$lib/components/same-toy/Screen.svelte'
   import Speaker from '$lib/components/same-toy/Speaker.svelte'
   import Start from '$lib/components/same-toy/Start.svelte'
@@ -14,7 +14,7 @@
   <div class="flex flex-col items-center gap-[6vh] flex-1">
     <Speaker class="flex-1" />
     <DPad class="flex-1" />
-    <PowerOn class="flex-1" />
+    <Ignition />
     <div class="flex-1"></div>
     <div class="flex-1"></div>
   </div>
